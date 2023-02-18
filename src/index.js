b5494869587526bad71e6c1350d24301325fcb15
happex <110610727+ha2pex@users.noreply.github.com>
2023-02-18 17:54:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 function load(page) {
 	// Page wrapper
 	const navbar = document.querySelector('#navigation');
-	navbar.classList.add('flex', 'bg-blue-600', 'border-b', 'border-b-2', 'border-b-slate-100/25', 'w-full', 'h-16', 'px-8', 'fixed', 'top-0', 'md:px-36'); 
+	navbar.classList.add('flex', 'bg-blue-600', 'border-b', 'border-b-2', 'border-b-slate-100/25', 'w-full', 'h-16', 'px-8', 'fixed', 'top-0', 'md:px-36', 'dark:bg-blue-800'); 
 	const navbarContent = `
 	<button class="h-full py-auto"><a href="/"><img src="/static/assets/logo.png" class="w-10 h-auto rounded-md shadow-lg" /></a></button>
-	<button class="h-full"><button class="bg-white text-sm font-medium h-8 px-5 mx-4 my-auto rounded-full"><a class="bg-gradient-to-br from-blue-600 via-indigo-500 to-fuchsia-600 bg-clip-text text-transparent" href="./join.html">Join</a></button></button>
+	<button class="h-full"><button class="bg-white text-sm font-medium h-8 px-7 mx-3 my-auto rounded-full shadow-lg"><a class="bg-gradient-to-br from-blue-600 via-indigo-500 to-fuchsia-600 bg-clip-text text-transparent" href="./join.html">Join</a></button></button>
 	<button class="h-full px-2 py-auto hover:bg-slate-50/5 md:hidden hover:border-b-4 hover:border-b-blue-500"><a href="./about.html"><img src="/static/icons/users.svg" class="w-8" /></a></button>
 	<button class="h-full px-2 py-auto hover:bg-slate-50/5 md:hidden hover:border-b-4 hover:border-b-blue-500"><a href="./terms.html"><img src="/static/icons/terms.svg" class="w-8" /></a></button>
 	<button class="h-full px-2 py-auto hover:bg-slate-50/5 md:hidden hover:border-b-4 hover:border-b-blue-500"><a href="./infos.html"><img src="/static/icons/info.svg" class="w-8" /></a></button>
