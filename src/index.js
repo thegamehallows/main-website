81a73440a3215f574bb1336d5f34f8c4390f4712
happex <110610727+ha2pex@users.noreply.github.com>
2023-02-19 20:27:52 +0100

    Add main page & custom font

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+import { pages } from './pages/app.js'
 const express = require('express')
 const app = express()
+const bridge = new Bridge('title')
+
 const content = document.querySelector('#app')
 
 app.get('/', (req, res) => {
-    content.innerHTML = mainPage
+    bridge.cross({title: 'The Game Hallows - Welcome!'})
+    document.querySelector('meta[property=\'og:title\']').setAttribute('content', 'The Game Hallows - Welcome!')
+    document.querySelector('meta[property=\'og:description\']').setAttribute('content', 'Just a group of gamers who stream and make games with the resources they have.')
+    document.querySelector('meta[property=\'og:color\']').setAttribute('content', '#a21caf')
+    document.querySelector('meta[property=\'og:imagr\']').setAttribute('content', '/public/static/banner.png')
+    content.innerHTML = pages.main
 })
