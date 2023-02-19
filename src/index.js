21b920148ee27307b516bf83cfe243113ce4afae
happex <110610727+ha2pex@users.noreply.github.com>
2023-02-19 20:37:40 +0100

    Add other pages

--- src/index.js
+++ src/index.js
@@ -13,3 +13,30 @@ app.get('/', (req, res) => {
     document.querySelector('meta[property=\'og:imagr\']').setAttribute('content', '/public/static/banner.png')
     content.innerHTML = pages.main
 })
+
+app.get('/about', (req, res) => {
+    bridge.cross({title: 'The Game Hallows - About'})
+    document.querySelector('meta[property=\'og:title\']').setAttribute('content', 'The Game Hallows - About')
+    document.querySelector('meta[property=\'og:description\']').setAttribute('content', 'About the Game Hallows')
+    document.querySelector('meta[property=\'og:color\']').setAttribute('content', '#a21caf')
+    document.querySelector('meta[property=\'og:imagr\']').setAttribute('content', '/public/static/banner.png')
+    content.innerHTML = pages.about
+})
+
+app.get('/infos', (req, res) => {
+    bridge.cross({title: 'The Game Hallows - Informations'})
+    document.querySelector('meta[property=\'og:title\']').setAttribute('content', 'The Game Hallows - Informations')
+    document.querySelector('meta[property=\'og:description\']').setAttribute('content', 'Become an elite member!')
+    document.querySelector('meta[property=\'og:color\']').setAttribute('content', '#a21caf')
+    document.querySelector('meta[property=\'og:imagr\']').setAttribute('content', '/public/static/banner.png')
+    content.innerHTML = pages.infos
+})
+
+app.get('/join', (req, res) => {
+    bridge.cross({title: 'The Game Hallows - Join'})
+    document.querySelector('meta[property=\'og:title\']').setAttribute('content', 'The Game Hallows - Join')
+    document.querySelector('meta[property=\'og:description\']').setAttribute('content', 'Join the Game Hallows')
+    document.querySelector('meta[property=\'og:color\']').setAttribute('content', '#a21caf')
+    document.querySelector('meta[property=\'og:imagr\']').setAttribute('content', '/public/static/banner.png')
+    content.innerHTML = pages.join
+})
